1442bc8fcddcfebf23410c74f462c57a19f082a8
oh-yuya <happy.cccccc.lemon@gmail.com>
2020-11-19 00:06:59 +0900

    debug

--- myprotcol.go
+++ myprotcol.go
@@ -64,7 +64,7 @@ func (c *Client) Send() {
 }
 
 func (c *Client) ReadFile() {
-	for i:=0; i<1000; i++ {
+	for i:=0; i<350; i++ {
 		fmt.Printf("read data%d\n", i)
 		fileName := fmt.Sprintf("data/data%d", i)
 		data, err := ioutil.ReadFile(fileName)

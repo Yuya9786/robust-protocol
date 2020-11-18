60cdd622054af84ea9d71418114347c3075d463d
oh-yuya <happy.cccccc.lemon@gmail.com>
2020-11-19 00:02:17 +0900

    debug

--- myprotcol.go
+++ myprotcol.go
@@ -56,7 +56,7 @@ func (c *Client) Send() {
 				}
 				c.Conn.Write(c.Buf[packetIdent.Fileno][packetIdent.Offset])
 				//fmt.Println("1", packetIdent)
-				if packetIdent.Offset == 69 {
+				if packetIdent.Offset >= 69 {
 					c.Ch1 <- packetIdent
 				}
 		}
@@ -64,7 +64,7 @@ func (c *Client) Send() {
 }
 
 func (c *Client) ReadFile() {
-	for i:=0; i<310; i++ {
+	for i:=0; i<1000; i++ {
 		fmt.Printf("read data%d\n", i)
 		fileName := fmt.Sprintf("data/data%d", i)
 		data, err := ioutil.ReadFile(fileName)

f6a494551a561a28f7e52d1944841d2486c63288
oh-yuya <happy.cccccc.lemon@gmail.com>
2020-11-19 03:23:52 +0900

    debug

--- myprotcol.go
+++ myprotcol.go
@@ -165,14 +165,14 @@ func (s *Server) Receive() {
 func (s *Server) handleClient() {
 	for {
 		buf := <- s.Ch
-		var packet *Packet
+		var packet Packet
 		fmt.Println(buf)
 		err := packet.Deserialize(buf)
 		if err != nil {
 			panic(err)
 		}
-		s.Bfp.Set(packet)
-		s.Ack(packet)
+		s.Bfp.Set(&packet)
+		s.Ack(&packet)
 	}
 }
 
